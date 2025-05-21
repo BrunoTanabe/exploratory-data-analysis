30d41d0dedd4731516c70f9bb99e984125a69dfe
Bruno Tanabe <tanabebruno@gmail.com>
2025-05-20 23:14:33 -0300

    [BUILD]: Implement dataset preparation class and update dependencies

--- src/preparing_dataset.py
+++ src/preparing_dataset.py
@@ -1,5 +1,68 @@
+from pathlib import Path
+
 import pandas as pd
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+"""
+    The Class preparing_dataset.py.
+    The preparing_dataset.py class is responsible for preparing the dataset for analysis.
+    @Author Bruno Tanabe
+    @CreatedAt 2025-04-23
+"""
+
+
 class PreparingDataset:
-    def __init__(self, dataset):
-        self.dataset = dataset
+
+    def __init__(self):
+        try:
+            self.df_churn = (
+                pd.read_csv(str(BASE_DIR) + "/data/churn_customers.csv")
+                .merge(
+                    pd.read_csv(str(BASE_DIR) + "/data/churn_services.csv"), on="customerID"
+                )
+                .merge(
+                    pd.read_csv(str(BASE_DIR) + "/data/churn_contracts.csv"),
+                    on="customerID",
+                )
+            )
+
+            self.df_churn["TotalCharges"] = pd.to_numeric(
+                self.df_churn["TotalCharges"], errors="coerce"
+            )
+
+            self.df_churn.columns = [
+                "customer_id",
+                "gender",
+                "senior_citizen",
+                "partner",
+                "dependents",
+                "phone_service",
+                "multiple_lines",
+                "internet_service",
+                "online_security",
+                "online_backup",
+                "device_protection",
+                "tech_support",
+                "streaming_tv",
+                "streaming_movies",
+                "tenure",
+                "contract",
+                "paperless_billing",
+                "payment_method",
+                "monthly_charges",
+                "total_charges",
+                "churn",
+            ]
+
+        except FileNotFoundError as e:
+            print(f"File not found: {e}")
+        except pd.errors.EmptyDataError as e:
+            print(f"Empty data error: {e}")
+        except pd.errors.ParserError as e:
+            print(f"Parser error: {e}")
+        except Exception as e:
+            print(f"An error occurred: {e}")
+
+            
+            
